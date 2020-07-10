7d2153189f757b7280bfdafbba572a2395a280b8
Herick Exterkoetter <herickherick47@gmail.com>
2020-07-10 01:53:32 -0300

    Conectando com o Banco de Dados

---
+++ src/database/migrations/1594111292433-CreateAppointments.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateAppointments1594111292433
+    implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'appointments',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'varchar',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+                    {
+                        name: 'provider',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'date',
+                        type: 'timestamp with time zone', // se não estiver no postgres deverá usar timestamp
+                        isNullable: false,
+                    },
+                ],
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('appointments');
+    }
+}
