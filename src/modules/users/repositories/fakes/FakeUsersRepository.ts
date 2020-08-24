9ada2a6cf6cfc5bcf48c37bf5d5022d57360ce54
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-23 22:21:25 -0300

    testes para criaçao do usuario

---
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,45 @@
+import { uuid } from 'uuidv4';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+
+import User from '../../infra/typeorm/entities/User';
+// Repository possui os metedos do typeORM de criar deletar e etc, recebendo o model como parametro
+
+class UsersRepository implements IUsersRepository {
+    private users: User[] = [];
+
+    public async findById(id: string): Promise<User | undefined> {
+        const findUser = this.users.find(user => user.id === id);
+
+        return findUser;
+    }
+
+    public async findByEmail(email: string): Promise<User | undefined> {
+        const findUser = this.users.find(user => user.email === email);
+
+        return findUser;
+    }
+
+    public async create(userData: ICreateUserDTO): Promise<User> {
+        const user = new User();
+
+        Object.assign(user, { id: uuid() }, userData);
+
+        this.users.push(user);
+
+        return user;
+    }
+
+    public async save(user: User): Promise<User> {
+        const findIndex = this.users.findIndex(
+            findUser => findUser.id === user.id,
+        );
+
+        this.users[findIndex] = user;
+
+        return user;
+    }
+}
+
+export default UsersRepository;
