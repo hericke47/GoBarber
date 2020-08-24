0cbcce8f75d27ed509fdbeaa981ffd007c3a4c45
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-23 21:49:22 -0300

    criando primeiro teste unitario

---
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,34 @@
+import { uuid } from 'uuidv4';
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+
+import Appointment from '../../infra/typeorm/entities/Appointment';
+// Repository possui os metedos do typeORM de criar deletar e etc, recebendo o model como parametro
+
+class AppointmentsRepository implements IAppointmentsRepository {
+    private appointments: Appointment[] = [];
+
+    public async findByDate(date: Date): Promise<Appointment | undefined> {
+        const findAppointment = this.appointments.find(
+            appointments => appointments.date === date,
+        );
+
+        return findAppointment;
+    }
+
+    public async create({
+        date,
+        provider_id,
+    }: ICreateAppointmentDTO): Promise<Appointment> {
+        const appointment = new Appointment();
+
+        Object.assign(appointment, { id: uuid(), date, provider_id });
+
+        this.appointments.push(appointment);
+
+        return appointment;
+    }
+}
+
+export default AppointmentsRepository;
