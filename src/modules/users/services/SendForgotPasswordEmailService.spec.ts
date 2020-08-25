376765eb1d243bbc819680fccf616f3b78e022a6
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-25 16:52:07 -0300

    fazendo o teste passar da maneira mais simples possivel

--- src/modules/users/services/SendForgotPasswordEmailService.spec.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -13,6 +13,7 @@ describe('SendForgotPasswordEmail', () => {
 
         const SendForgotPasswordEmail = new SendForgotPasswordEmailService(
             fakeUsersRepository,
+            fakeMailProvider,
         );
 
         await fakeUsersRepository.create({
