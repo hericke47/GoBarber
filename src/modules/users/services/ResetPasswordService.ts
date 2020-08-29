3089fa984bcd55db453ebea10c6b2800a669e961
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-29 11:07:51 -0300

    hash nos testes

--- src/modules/users/services/ResetPasswordService.ts
+++ src/modules/users/services/ResetPasswordService.ts
@@ -4,6 +4,7 @@ import { injectable, inject } from 'tsyringe';
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IUserTokensRepository from '../repositories/IUserTokensRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 // import User from '../infra/typeorm/entities/User';
 
@@ -20,6 +21,9 @@ class ResetPasswordService {
 
         @inject('UserTokensRepository')
         private userTokensRepository: IUserTokensRepository,
+
+        @inject('HashProvider')
+        private hashProvider: IHashProvider,
     ) {}
 
     public async execute({ token, password }: IRequest): Promise<void> {
@@ -35,7 +39,7 @@ class ResetPasswordService {
             throw new AppError('User does not exists');
         }
 
-        user.password = password;
+        user.password = await this.hashProvider.generateHash(password);
 
         await this.usersRepository.save(user);
     }
