9190fed6a6048d6330faae4941a5058042be3df1
Herick Exterkoetter <herickherick47@gmail.com>
2020-08-30 23:57:05 -0300

    teste para reset de password apos duas horas, dando erro pois ainda n foi feito a logica

--- src/modules/users/services/ResetPasswordService.spec.ts
+++ src/modules/users/services/ResetPasswordService.spec.ts
@@ -23,7 +23,7 @@ describe('ResetPasswordService', () => {
         );
     });
 
-    it('it shoul be able to reset the password', async () => {
+    it('shoul be able to reset the password', async () => {
         const user = await fakeUsersRepository.create({
             name: 'John Doe',
             email: 'johndoe@example.com',
@@ -63,6 +63,29 @@ describe('ResetPasswordService', () => {
             resetPassword.execute({
                 token,
                 password: '123456',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('should not be able to reset password if passed more than 2 hours', async () => {
+        const user = await fakeUsersRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123456',
+        });
+
+        const { token } = await fakeUserTokensRepository.generate(user.id);
+
+        jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+            const customDate = new Date();
+
+            return customDate.setHours(customDate.getHours() + 3);
+        });
+
+        await expect(
+            resetPassword.execute({
+                token,
+                password: '123123',
             }),
         ).rejects.toBeInstanceOf(AppError);
     });
