9a93194d29abb417ce81bd6ad2b8d89fad6a0138
Herick Exterkoetter <herickherick47@gmail.com>
2020-09-07 19:23:02 -0300

    criaçao do agendamento

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -13,6 +13,7 @@ import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
     provider_id: string;
+    user_id: string;
     date: Date;
 }
 
@@ -29,6 +30,7 @@ class CreateAppointmentService {
     public async execute({
         date,
         provider_id,
+        user_id,
     }: IRequest): Promise<Appointment> {
         const appointmentDate = startOfHour(date);
 
@@ -42,6 +44,7 @@ class CreateAppointmentService {
 
         const appointment = await this.appointmentsRepository.create({
             provider_id,
+            user_id,
             date: appointmentDate,
         });
 
