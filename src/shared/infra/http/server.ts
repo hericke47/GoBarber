48f6f21fc20e66c165cfb224eb510fc389468186
Herick Exterkoetter <herickherick47@gmail.com>
2020-09-09 00:25:22 -0300

    validando dados nas rotas

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import 'express-async-errors';
 
 import uploadConfig from '@config/upload';
@@ -16,6 +17,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
+
+app.use(errors());
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use(
